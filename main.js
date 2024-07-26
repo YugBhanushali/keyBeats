a9114b7f786e980f99ddc8e0b109a37e5361a99d
YugBhanushali <yughbhanushali241103@gmail.com>
2024-07-26 12:24:11 +0530

    some changes in the menu.html and deleted some files

--- main.js
+++ main.js
@@ -1,113 +1,4 @@
-// const { app, BrowserWindow, ipcMain, Menu } = require("electron");
-// const { menubar } = require("menubar");
-// const path = require("path");
-// const { keySounds } = require("./audioFiles/audioModules/audioModule");
-
-// let mainWindow;
-// let currentSoundSet = "alpaca";
-// let currentVolume = 15;
-
-// const mb = menubar({
-//   index: "file://" + path.join(__dirname, "volume.html"),
-//   icon: path.join(__dirname, "assets", "images", "keyBeats@2x.png"),
-//   preloadWindow: true,
-//   browserWindow: {
-//     width: 300,
-//     height: 400,
-//     webPreferences: {
-//       nodeIntegration: true,
-//       contextIsolation: false,
-//     },
-//   },
-// });
-
-// mb.on("ready", () => {
-//   console.log("Menubar app is ready");
-//   createMenu();
-// });
-
-// function createMenu() {
-//   const contextMenu = Menu.buildFromTemplate([
-//     {
-//       label: "Sound Sets",
-//       submenu: keySounds.map((set) => ({
-//         label: set.caption,
-//         type: "radio",
-//         checked: set.key === currentSoundSet,
-//         click: () => {
-//           currentSoundSet = set.key;
-//           mainWindow.webContents.send("change-sound-set", set.key);
-//         },
-//       })),
-//     },
-//     { type: "separator" },
-//     {
-//       label: "Volume Control",
-//       submenu: [
-//         {
-//           label: `Volume: ${currentVolume}`,
-//           enabled: false,
-//           id: "volume-label",
-//         },
-//         {
-//           label: "Increase Volume",
-//           click: (menuItem, browserWindow, event) => {
-//             event.preventDefault();
-//             if (currentVolume < 20) {
-//               currentVolume++;
-//               updateVolumeLabel(menuItem.menu);
-//               mainWindow.webContents.send("change-volume", currentVolume);
-//             }
-//           },
-//         },
-//         {
-//           label: "Decrease Volume",
-//           click: (menuItem, browserWindow, event) => {
-//             event.preventDefault();
-//             if (currentVolume > 10) {
-//               currentVolume--;
-//               updateVolumeLabel(menuItem.menu);
-//               mainWindow.webContents.send("change-volume", currentVolume);
-//             }
-//           },
-//         },
-//       ],
-//     },
-//     { type: "separator" },
-//     { label: "Quit", click: () => app.quit() },
-//   ]);
-
-//   mb.tray.setContextMenu(contextMenu);
-// }
-
-// function updateVolumeLabel(menu) {
-//   const volumeLabel = menu.getMenuItemById("volume-label");
-//   if (volumeLabel) {
-//     volumeLabel.label = `Volume: ${currentVolume}`;
-//   }
-//   createMenu(); // Refresh the entire menu
-// }
-
-// ipcMain.on("sound-set-changed", (event, newSoundSet) => {
-//   currentSoundSet = newSoundSet;
-//   createMenu();
-// });
-
-// mb.on("after-create-window", () => {
-//   mainWindow = mb.window;
-
-//   // if (process.env.NODE_ENV === "development") {
-//   //   mainWindow.openDevTools();
-//   // }
-// });
-
-// app.on("window-all-closed", () => {
-//   if (process.platform !== "darwin") {
-//     app.quit();
-//   }
-// });
-
-const { app, BrowserWindow, ipcMain } = require("electron");
+const { app, BrowserWindow, ipcMain, nativeImage } = require("electron");
 const { menubar } = require("menubar");
 const path = require("path");
 
@@ -123,9 +14,26 @@ if (isDev) {
 
 let mainWindow;
 
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 700,
+    height: 600,
+    show: true, // Changed to true
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
+  });
+  mainWindow.loadFile("index.html");
+  mainWindow.webContents.openDevTools(); // Open DevTools for debugging
+}
+
+let trayImage = nativeImage.createFromPath(
+  path.join(__dirname, "assets", "images", "keyBeats@2x.png")
+);
 const mb = menubar({
-  index: "file://" + path.join(__dirname, "test.html"),
-  icon: path.join(__dirname, "assets", "images", "keyBeats@2x.png"),
+  index: "file://" + path.join(__dirname, "menu.html"),
+  icon: trayImage.resize({ height: 16, width: 16 }),
   preloadWindow: true,
   browserWindow: {
     width: 260,
@@ -147,6 +55,10 @@ mb.on("after-create-window", () => {
   if (process.env.NODE_ENV === "development") {
     // mainWindow.openDevTools();
   }
+});
+
+app.whenReady().then(() => {
+  createWindow();
 });
 
 ipcMain.on("quit-app", () => {
